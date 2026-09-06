72d1384de848fb23d0794583f9767c3374158313
wildlh <17820091366@163.com>
2026-09-06 14:59:11 +0800

    添加Olivia名字，将Olivia修改为alex模型

--- src/main/java/dev/efm/solaris_compat/client/OliviaRenderer.java
+++ src/main/java/dev/efm/solaris_compat/client/OliviaRenderer.java
@@ -4,7 +4,6 @@ import dev.efm.solaris_compat.SolarisCompat;
 import dev.efm.solaris_compat.common.entity.OliviaEntity;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.resources.ResourceLocation;
@@ -15,7 +14,7 @@ public class OliviaRenderer extends HumanoidMobRenderer<OliviaEntity, PlayerMode
             ResourceLocation.fromNamespaceAndPath(SolarisCompat.MODID, "textures/entity/olivia.png");
 
     public OliviaRenderer(EntityRendererProvider.Context context) {
-        super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER), false), 0.5F);
+        super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER_SLIM), true), 0.5F);
     }
 
     @Override
